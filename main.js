d7047a3fc5169fbca032a0e64984992b199428f4
nisol91 <nicolasolzi91@gmail.com>
2018-12-03 15:48:57 +0100

    8 - js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     'testi' : ['', 'Ciao, come va?', 'Domani a che ora ci troviamo?', 'Buonasera', 'Buonanotte', 'Posso chiamarti? :)', 'Ottimo!!'],
     'immagine' : ['', "https://images.wallpaperscraft.com/image/mountains_clouds_trees_snow_119169_3840x2160.jpg", "https://images.wallpaperscraft.com/image/mountains_starry_sky_milky_way_night_119973_3840x2160.jpg", "https://images.wallpaperscraft.com/image/mountains_trees_frozen_lake_winter_landscape_93344_3840x2160.jpg", "https://images.wallpaperscraft.com/image/mountains_landscape_mt_rainier_washington_grass_hdr_90605_3840x2160.jpg", "https://images.wallpaperscraft.com/image/mountains_cordillera_sky_sunset_sun_clouds_95556_3840x2160.jpg"],
     'orario' : ['', '12:50', '18:42', '15:30', '10:59', '14:02'],
-    'stato' : ['', 'Online', 'ultimo accesso alle ore ' + orari_accesso[random_int_number(0, 5)], 'Sta scrivendo']
+    'stato' : ['', 'Online', 'ultimo accesso alle ore ' + orari_accesso[random_int_number(0, 4)], 'Sta scrivendo']
   };
   var num_cont = contatto.numero;
   console.log(contatto.nome[2]);
@@ -39,6 +39,8 @@ $(document).ready(function() {
 
 
   $('.con-item').click(function(event) {
+    $('.messages').show();
+    $('.logo-rimpiazzo').hide();
     var nome = $(this).find('h3').text();
     var scritte = $(this).find('h4').text();
     var ora = $(this).find('p').text();
@@ -61,17 +63,42 @@ $(document).ready(function() {
 
 //inserisci messaggi verdi o bianchi
 
- $('.invio').click(function() {
-   var mio_mex = $('.form-control-i').val();
-   console.log(mio_mex);
-   $('.messages .mess-output p').text(mio_mex);
-   var d = new Date();
-   var n = d.getHours();
-   var m = d.getMinutes();
-   console.log(n);
-   $('.messages .mess-output h5').text(n + ':' + m);
+
+  $('.invio').click(function() {
+    var mio_mex = $('.form-control-i').val();
+    console.log(mio_mex);
+    $('.messages .mess-output p').text(mio_mex);
+    $('.form-control-i').val('');
+
+    var d = new Date();
+    var n = d.getHours();
+    var m = d.getMinutes();
+    console.log(n);
+    $('.messages .mess-output h5').text(n + ':' + m);
+   });
+
+//inserisci tramite tastiera
+
+  var new_cont = $('.mess-output').clone()
+
+ $(document).keypress(function(event) {
+   if (event.which == 13) {
+     var copy = new_cont.clone();
+     $('.mess-main').append(copy);
+     copy.removeClass('nascondi');
+
+
+     var mio_mex = $('.form-control-i').val();
+     console.log(mio_mex);
+     $(copy).find('p').text(mio_mex);
+     $('.form-control-i').val('');
+
+     var d = new Date();
+     var n = d.getHours();
+     var m = d.getMinutes();
+     console.log(n);
+     $('.messages .mess-output h5').text(n + ':' + m);
+    }
+    console.log(event.which);
   });
-
-
-
 });
