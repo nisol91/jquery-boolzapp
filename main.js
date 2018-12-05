9ddda1e8927c3903f462f816d6c1165b906c612e
nisol91 <nicolasolzi91@gmail.com>
2018-12-05 16:13:19 +0100

    fix tendine e spunte a comparsa;

--- main.js
+++ main.js
@@ -56,18 +56,39 @@ var clone_3 = $('.mess-inp-contenitore').clone()
 
 
   $('.con-item').click(function(event) {
-
     $('.mess-inp-contenitore').remove()//reinizializza i messaggi ricevuti cancellandoli
     $('.con-item').removeClass('lightgrey')
     $(this).addClass('lightgrey');
 
-    $('.mess-inp-contenitore').show();//reinizializza i messaggi cancellati permettendo nuovamente di vederli
     $('.mess-out-contenitore').remove();//reinizializza i messaggi scritti cancellandoli
+    $('.mess-inp-contenitore .con-item').show();//reinizializza i messaggi cancellati permettendo nuovamente di vederli
+
 
     // console.log(copy_3);
-    var copy_3 = $(clone_3).clone()//anche qui devo fare cosi perche cosi ogni volta ho un modello vuoto: e' il mio modello vuoto copiato dal template, poi a ogni azione(click,mouseenter..) lo copio per poi riempirlo
-    //in alternativa posso usare il metodo clear template, ovvero appendere il div template vuoto alla fine. Penso sia analogo
-    $('.mess-main').append(copy_3);
+    // var copy_3 = $(clone_3).clone()//anche qui devo fare cosi perche cosi ogni volta ho un modello vuoto: e' il mio modello vuoto copiato dal template, poi a ogni azione(click,mouseenter..) lo copio per poi riempirlo
+    // $('.mess-main').append(copy_3);
+    //
+    // $('.messages').show();
+    // $('.logo-rimpiazzo').hide();
+    // var nome = $(this).find('h3').text();
+    // var scritte = $(this).find('h4').text();
+    // var ora = $(this).find('p').text();
+    // var immagine_prof = $(this).find('img').attr('src');
+    // var state = contatto.stato[random_int_number(0, 3)];
+    //
+    // console.log(nome);
+    // console.log(scritte);
+    // $('.messages h3').text(nome);
+    // $(copy_3).find('p').text(scritte);
+    // $(copy_3).find('h5').text(ora);
+    // $('.messages .text-person p').text(state);
+    // $('.messages .prof-pic img').attr('src', immagine_prof);
+
+
+//in alternativa posso usare il metodo clear template, ovvero appendere il div template vuoto alla fine. Penso sia analogo
+//ricorda di aggiungere il div clear(vuoto) nel template
+
+    $('.mess-main').append(clone_3);
 
     $('.messages').show();
     $('.logo-rimpiazzo').hide();
@@ -80,11 +101,13 @@ var clone_3 = $('.mess-inp-contenitore').clone()
     console.log(nome);
     console.log(scritte);
     $('.messages h3').text(nome);
-    $(copy_3).find('p').text(scritte);
-    $(copy_3).find('h5').text(ora);
+    $(clone_3).find('p').text(scritte);
+    $(clone_3).find('h5').text(ora);
     $('.messages .text-person p').text(state);
     $('.messages .prof-pic img').attr('src', immagine_prof);
 
+    var clearTemplate = $('.template .cl-t').clone();
+    $('.mess-main').append(clearTemplate);
 
   });
 
@@ -110,12 +133,17 @@ var clone_2 = $('.mess-inp-contenitore').clone()
     console.log(mio_mex);
     $(copy).find('p').text(mio_mex);
     $('.form-control-i').val('');
+    $('.tendina_out').hide();//cosi ho eliminato la tendina all apertura delcontatto senza dover usare classe nascondi
 
     var d = new Date();
     var n = d.getHours();
     var m = d.getMinutes();
     console.log(n);
-    $('.messages .mess-output h5').text(n + ':' + m);
+    if (m >= 10) {
+      $('.messages .mess-output h5').text(n + ':' + m);
+    } else if (m < 10) {
+      $('.messages .mess-output h5').text(n + ':0' + m);
+    }
 
 //risposta CPU
 
@@ -132,8 +160,13 @@ var clone_2 = $('.mess-inp-contenitore').clone()
       var d = new Date();
       var n = d.getHours();
       var m = d.getMinutes();
-      console.log(n);
-      $('.messages .mess-input h5').text(n + ':' + m);
+      console.log(m);
+      if (m >= 10) {
+        $('.messages .mess-input h5').text(n + ':' + m);
+      } else if (m < 10) {
+        $('.messages .mess-input h5').text(n + ':0' + m);
+      }
+      $('.tendina_inp').hide();//cosi ho eliminato la tendina all apertura delcontatto senza dover usare classe nascondi
     }, 1000);
 
 
@@ -151,12 +184,18 @@ var clone_2 = $('.mess-inp-contenitore').clone()
      console.log(mio_mex);
      $(copy).find('p').text(mio_mex);
      $('.form-control-i').val('');
+     $('.tendina_out').hide();//cosi ho eliminato la tendina all apertura delcontatto senza dover usare classe nascondi
+
 
      var d = new Date();
      var n = d.getHours();
      var m = d.getMinutes();
-     console.log(n);
-     $('.messages .mess-output h5').text(n + ':' + m);
+     console.log(m);
+     if (m >= 10) {
+       $('.messages .mess-output h5').text(n + ':' + m);
+     } else if (m < 10) {
+       $('.messages .mess-output h5').text(n + ':0' + m);
+     }
 
 //risposta CPU
 
@@ -174,7 +213,12 @@ var clone_2 = $('.mess-inp-contenitore').clone()
        var n = d.getHours();
        var m = d.getMinutes();
        console.log(n);
-       $('.messages .mess-input h5').text(n + ':' + m);
+       if (m >= 10) {
+         $('.messages .mess-input h5').text(n + ':' + m);
+       } else if (m < 10) {
+         $('.messages .mess-input h5').text(n + ':0' + m);
+       }
+       $('.tendina_inp').hide();//cosi ho eliminato la tendina all apertura delcontatto senza dover usare classe nascondi
      }, 1000);
 
 
@@ -183,87 +227,129 @@ var clone_2 = $('.mess-inp-contenitore').clone()
   });
 
 //mostra spunta
+// quando appendi qualcosa e poi vuoi farci qualcosa dentro ( es appeno un div e poi a quel div voglio far apparire qualcosa dentro al click)
+// non posso usare il semplice $().click ma $(document).on(‘click’,’div’, function(){})
 
 
 
-  $('.mess-input').mouseenter(function() {
+
+  $(document).on('mouseenter', '.mess-input', function() {
     $(this).find('.fa-check').removeClass('nascondi');
   });
-  $('.mess-input').mouseleave(function() {
+  $(document).on('mouseleave', '.mess-input', function() {
     $(this).find('.fa-check').addClass('nascondi');
   });
 
-
-  $('.mess-output').mouseenter(function() {
+  $(document).on('mouseenter', '.mess-output', function() {
     $(this).find('.fa-check').removeClass('nascondi');
   });
-  $('.mess-output').mouseleave(function() {
+  $(document).on('mouseleave', '.mess-output', function() {
     $(this).find('.fa-check').addClass('nascondi');
   });
+
 
 //menu a tendina
-
-  $('.tendina_inp').hide();
-
-
-  $('.fa_inp').click(function() {
-    $('.tendina_inp').slideToggle('slow');
+  $(document).on('click', '.con-item', function(event) {//cosi ho eliminato la tendina all apertura delcontatto senza dover usare classe nascondi
+    $('.tendina_inp').hide();
   });
-  $('.tendina_inp').mouseleave(function() {
+
+  $(document).on('click', '.fa_inp', function() {
+    $(this).siblings('.tendina_inp').slideToggle('slow');
+    // console.log(event.target);
+  });
+  $(document).on('mouseleave', '.tendina_inp', function() {
     $('.tendina_inp').slideUp('slow');
   });
+
   //---
-  $('.tendina_out').hide();
 
-
-  $('.fa_ out').click(function() {
-    $('.tendina_out').slideDown('slow');
+  $(document).on('click', '.con-item', function(event) {
+    $('.tendina_out').hide();
   });
-  $('.tendina_out').mouseleave(function() {
+
+
+  $(document).on('click', '.fa_out', function() {
+    $(this).siblings('.tendina_out').slideToggle('slow');
+    // console.log(event.target);
+  });
+  $(document).on('mouseleave', '.tendina_out', function() {
     $('.tendina_out').slideUp('slow');
   });
 
+
 //delete message
 
-  $('.tendina_inp .delete').click(function() {
-    $(this).parent('.tendina_inp').parent('.mess-input').hide();
+  $(document).on('click', '.tendina_inp .delete', function() {
+    $(this).parent('.tendina_inp').parent('.mess-input').parent('.mess-inp-contenitore').hide();
   });
+
 //--
+
+  $(document).on('click', '.tendina_out .delete', function() {
+    $(this).parent('.tendina_out').parent('.mess-output').parent('.mess-out-contenitore').hide();
+  });
 
 
 //ricerca contatti (each() mi cicla su tutti i con-item)
 
 
 
-  $('.fa-search').click(function() {
-    var count = [];
-    $('.con-item').show();
-    $('.con-item').each(function() {
-      var writtenValue = $('.form-control').val();
-      var wVU = writtenValue.toUpperCase();
-      var nthContactName = $(this).find('h3').text();
-      var nCN = nthContactName.toUpperCase();
-      var check = nCN.includes(wVU);
+  // $('.fa-search').click(function() {
+  //   var count = [];
+  //   $('.con-item').show();
+  //   $('.con-item').each(function() {
+  //     var writtenValue = $('.form-control').val();
+  //     var wVU = writtenValue.toUpperCase();
+  //     var nthContactName = $(this).find('h3').text();
+  //     var nCN = nthContactName.toUpperCase();
+  //     var check = nCN.includes(wVU);
+  //
+  //     console.log(writtenValue);
+  //     console.log(nthContactName);
+  //     console.log(check);
+  //     if (check == false) {
+  //       $(this).hide();
+  //       console.log($(this).find('h3').text());
+  //     } else {
+  //       count.push('1');
+  //     }
+  //     console.log(count);
+  //   });
+  //   if (count.length == 0) {
+  //     // console.log('ups');
+  //     $('.conv-list').text('Nessun contatto corrispondente').css('font-size', '30px').css('text-align', 'center').css('color', 'grey');
+  //   }
+  //   // console.log($('.form-control').val());
+  // });
 
-      console.log(writtenValue);
-      console.log(nthContactName);
-      console.log(check);
-      if (check == false) {
-        $(this).hide();
-        console.log($(this).find('h3').text());
-      } else {
-        count.push('1');
-      }
-      console.log(count);
-    });
-    if (count.length == 0) {
-      // console.log('ups');
-      $('.conv-list').text('Nessun contatto corrispondente').css('font-size', '30px').css('text-align', 'center').css('color', 'grey');
+//in alternativa posso dirgli di applicare le mie regole al keydown
+
+$('.form-control').keyup(function() {
+  var count = [];
+  $('.con-item').show();
+  $('.con-item').each(function() {
+    var writtenValue = $('.form-control').val();
+    var wVU = writtenValue.toLowerCase();
+    var nthContactName = $(this).find('h3').text();
+    var nCN = nthContactName.toLowerCase();
+    var check = nCN.includes(wVU);
+
+    console.log(writtenValue);
+    console.log(nthContactName);
+    console.log(check);
+    if (check == false) {
+      $(this).hide();
+      console.log($(this).find('h3').text());
+    } else {
+      count.push('1');
     }
-    // console.log($('.form-control').val());
+    console.log(count);
   });
-
-
-
+  // if (count.length == 0) {
+  //   // console.log('ups');
+  //   $('.conv-list').text('Nessun contatto corrispondente').css('font-size', '30px').css('text-align', 'center').css('color', 'grey');
+  // }
+  console.log($('.form-control').val());
+});
 
 });
